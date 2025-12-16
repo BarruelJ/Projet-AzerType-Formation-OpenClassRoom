32a555783c905b9e7577eea8634e400c9e896e46
Joan Barruel <j.barruel@icloud.com>
2025-12-16 17:04:16 +0100

    Supression des méthodes prompt, nettoyage du code, ajout d'une interaction avec la balise input et le bouton valider

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,3 @@
-function choisirPhrasesOuMots() {
-
-    let choix = prompt("Veuillez choix la liste : Mots ou Phrases")
-    while (choix !== "Mots" && choix !== "Phrases") {
-        choix = prompt("Veuillez choix la liste : Mots ou Phrases")
-    }
-    return choix
-}
-
 function afficherResultat(score, total) {
     let div = document.querySelector(".affichageScore p")
     let affichageScore = `${score} / ${total} `
@@ -14,31 +5,18 @@ function afficherResultat(score, total) {
     div.innerText = affichageScore
 }
 
-function lancerBoucleDeJeu (listProposition){
-    let score = 0
-    for (let i = 0; i < listProposition.length; i++) {
-        let motUtilisateur = prompt("Entrez le mot " + listProposition[i])
-        if (motUtilisateur === listProposition[i]) {
-            score++
-        }
-        else {
-            console.log("Vous avez écrit " + motUtilisateur + " au lieu de " + listProposition[i])
-        }
-    }
-    return score
-}
 
 function lancerJeu() {
 
-    let choix = choisirPhrasesOuMots()
+
     let score = 0
-    if (choix === "Mots"){
-        score = lancerBoucleDeJeu(listMots)
-        total = listMots.length
-    }
-    else  {
-        score = lancerBoucleDeJeu(listPhrases)
-        total = listPhrases.length
-    }
+    let total = 0
+
+    let boutonValider = document.querySelector(".reponseUser button")
+    boutonValider.addEventListener("click", () => {
+        console.log(inputEcriture.value)
+    })
     afficherResultat(score, total)
 }
+
+let inputEcriture = document.getElementById("inputEcriture")
