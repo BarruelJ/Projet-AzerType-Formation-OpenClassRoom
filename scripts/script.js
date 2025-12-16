f161666a227263b304107dbf9a66a7a6dbc8cea3
Joan Barruel <j.barruel@icloud.com>
2025-12-17 00:14:31 +0100

    ajout d'un if pour comparé la valeur de l'utilisateur a la liste de mots. Ajout du score et du total de mot proposé.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 function afficherResultat(score, total) {
-    let div = document.querySelector(".affichageScore p")
+    let div = document.querySelector(".affichageScore span p")
     let affichageScore = `${score} / ${total} `
 
     div.innerText = affichageScore
@@ -7,7 +7,7 @@ function afficherResultat(score, total) {
 
 function ecritureValue (input) {
 
-    console.log(input.value)
+    return input.value
     // input.value = "" //Dans le corrigé, le clear de l'input est intégrer dans le if.
 }
 
@@ -21,16 +21,22 @@ function lancerJeu() {
 
     let i = 0
     let score = 0
-    let total = 0
     let inputEcriture = document.getElementById("inputEcriture")
+   
     
     afficherProposition(listMots[i])
     let boutonValider = document.querySelector(".reponseUser button")
     boutonValider.addEventListener("click", () => {
-        ecritureValue(inputEcriture)
-        i++
+        const valeurUtilisateur = ecritureValue(inputEcriture)
         inputEcriture.value = ''
-        
+
+
+        if(listMots[i] === valeurUtilisateur) {
+            score++
+        }
+
+        i++
+        afficherResultat(score, i)
         if (listMots[i] === undefined) {
             let div = document.querySelector(".motProposition")
             div.innerText = ("Jeu Terminé !")
@@ -38,10 +44,13 @@ function lancerJeu() {
             inputEcriture.disabled = true
         } else {
         afficherProposition(listMots[i])}
+
+
+        
         
     })
-    afficherResultat(score, total)
     
+    afficherResultat(score, i)
 }
 
 
